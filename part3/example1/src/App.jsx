03ea6e2d2b983f0a6bd79b968a4cadf2e5f3e703
Daniel P <mar1ondanie1@outloook.com>
2024-06-12 22:30:25 -0600

    commit

---
+++ part3/example1/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
+function App({notes}) {
+
+ 
+
+  return (
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map(note =>
+            <li key={note.id}>
+              {note.content}
+            </li>          
+       )}
+      </ul>
+    </>
+  )
+}
+
+export default App
